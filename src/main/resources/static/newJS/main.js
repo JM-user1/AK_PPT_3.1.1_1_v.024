82d952121e8b9cd06a0369e615b05ae7b7e2f051
GALAXARA <asyakuzinaasyakuzina@gmail.com>
2021-04-18 11:57:52 +0700

    Изменил main.js if (text === 'Edit') {

--- src/main/resources/static/newJS/main.js
+++ src/main/resources/static/newJS/main.js
@@ -11,21 +11,23 @@ $(document).ready(function() {
     // console.log(roles);
     $('.table .eBtn').on('click', function(event) {
         event.preventDefault();
-        var href = $(this).attr('href');
+        const href = $(this).attr('href');
+        const text = $(this).text();
+        if (text === 'Edit') {
+            $.get(href, function (user, status) {
+                console.log(user)
 
+                $('.editModalForm #modalId').val(user.id);
+                $('.editModalForm #modalName').val(user.firstname);
+                $('.editModalForm #modalLastName').val(user.lastname);
+                $('.editModalForm #modalAge').val(user.age);
+                $('.editModalForm #modalEmail').val(user.email);
+                $('.editModalForm #modalUsername').val(user.email);
+                $('.editModalForm #modalPassword').val();
+                $('.editModalForm #modalRoles').val(user.roles);
+            });
 
-        $.get(href,function (user,status){
-            console.log(user)
-            $('.editModalForm #modalId').val(user.id);
-            $('.editModalForm #modalName').val(user.firstname);
-            $('.editModalForm #modalLastName').val(user.lastname);
-            $('.editModalForm #modalAge').val(user.age);
-            $('.editModalForm #modalEmail').val(user.email);
-            $('.editModalForm #modalUsername').val(user.email);
-            $().val(user.password);
-            $('.editModalForm #modalRoles').val(user.roles);
-        });
-
-        $('.editModalForm #editModal').modal('show');
+            $('.editModalForm #editModal').modal('show');
+        }
     });
 });
